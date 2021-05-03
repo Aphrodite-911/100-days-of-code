77dd53a158fed4c4b2456de423931bcc6e14579d
Aphrodite-911 <pimlada.ct@gmail.com>
2021-05-03 01:40:31 -0700

    form validation

---
+++ day1-form-validation/script.js
@@ -0,0 +1,13 @@
+const firstNameEl = document.querySelector('#firstName');
+const lastNameEl = document.querySelector('#lastName');
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#confirm-password');
+
+const form = document.querySelector('#signup');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+});
